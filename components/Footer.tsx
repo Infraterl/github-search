3b717e074d764c4bd2412490ca0ab0e40755e007
Babatunde Ademilua <babatunde@babatundes-air.mynet>
2022-04-09 13:42:05 +0100

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,11 +4,7 @@ import styles from "styles/Home.module.css";
 export default function Footer() {
   return (
     <footer className="text-center mt-5">
-      Powered by{" "}
-      <span className={styles.logo}>
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </span>
-      <div>Made by Ernest</div>
+      <div>Made by Babatunde</div>
     </footer>
   );
 }
